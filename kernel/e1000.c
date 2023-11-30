28077bdbe4b9497798c8d792eeab141228a537e4
silver-ymz <yinmingzhuo@gmail.com>
2023-11-30 11:45:29 +0800

    complete all
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -102,7 +102,21 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+
+  acquire(&e1000_lock);
+  uint32 tail = regs[E1000_TDT];
+  if (!(tx_ring[tail].status & E1000_TXD_STAT_DD)) {
+    release(&e1000_lock);
+    return -1;
+  }
+  if (tx_mbufs[tail])
+    mbuffree(tx_mbufs[tail]);
+  tx_mbufs[tail] = m;
+  tx_ring[tail].addr = (uint64)m->head;
+  tx_ring[tail].length = m->len;
+  tx_ring[tail].cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+  regs[E1000_TDT] = (tail + 1) % TX_RING_SIZE;
+  release(&e1000_lock);
   return 0;
 }
 
@@ -115,6 +129,20 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+
+  uint32 tail = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+  while (rx_ring[tail].status & E1000_RXD_STAT_DD) {
+    struct mbuf *m = rx_mbufs[tail];
+    m->len = rx_ring[tail].length;
+    net_rx(m);
+    rx_mbufs[tail] = mbufalloc(0);
+    if (!rx_mbufs[tail])
+      panic("e1000");
+    rx_ring[tail].addr = (uint64) rx_mbufs[tail]->head;
+    rx_ring[tail].status = 0;
+    regs[E1000_RDT] = tail;
+    tail = (tail + 1) % RX_RING_SIZE;
+  }
 }
 
 void
